7fe4aaaa434f98ad0d614adee95fb024caaeddd7
Mike Boan <mikeboan@gmail.com>
2019-02-13 14:35:51 -0500

    wip: pass tests, remove warnings

--- test/form-field.directive.spec.ts
+++ test/form-field.directive.spec.ts
@@ -3,9 +3,9 @@ import { ComponentFixture, TestBed, fakeAsync, tick } from "@angular/core/testin
 import { By } from "@angular/platform-browser";
 
 import { Validator } from "@lchemy/model/validation";
-import { FormFieldDirective } from "@lchemy/ng-forms/form-field.directive";
-import { FormComponent } from "@lchemy/ng-forms/form.component";
-import { FormsModule } from "@lchemy/ng-forms/forms.module";
+import { FormFieldDirective } from "../src/form-field.directive";
+import { FormComponent } from "../src/form.component";
+import { FormsModule } from "../src/forms.module";
 
 import { User, userValidator } from "./fixtures/user";
 
